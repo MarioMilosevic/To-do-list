27d256026a100224e34a53bf2a8e561e5e2fd3fc
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-26 12:47:49 +0100

    Add constants and helpers file

---
+++ constants.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export function init() {
+  const input = document.querySelector(".input");
+  const btn = document.querySelector(".btn");
+
+  return {
+    input,
+    btn,
+  };
+}
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-'use strict'
+"use strict";
+import { init } from "./constants";
+
+const { btn, input } = init();
+
+class ToDoList {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+btn.addEventListener("click", function () {
+  let inputText = input.value;
+  const task = new ToDoList(inputText);
+  console.log(task);
+//   inputText = "";
+  console.log(input);
+});
