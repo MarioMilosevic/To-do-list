83644566fb1a36a062c17d87699892666d586a49
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-27 12:10:10 +0100

    Add edit button into tasks

--- helpers.js
+++ helpers.js
@@ -3,20 +3,23 @@
 export const createDiv = (obj, parent, arr) => {
   const taskDiv = document.createElement("div");
   taskDiv.textContent = obj.text;
-  const taskBtn = document.createElement("button");
   taskDiv.classList.add("taskDiv");
-  taskBtn.classList.add("taskBtn");
-  taskBtn.textContent = 'X';
-  taskDiv.appendChild(taskBtn);
+  const taskRemoveBtn = document.createElement("button");
+  taskRemoveBtn.classList.add("taskRemoveBtn");
+  taskRemoveBtn.textContent = "X";
+  const taskEditBtn = document.createElement("button");
+  taskEditBtn.classList.add("taskEditBtn");
+  taskEditBtn.textContent = "Edit";
+  taskDiv.appendChild(taskRemoveBtn);
+  taskDiv.appendChild(taskEditBtn);
   parent.appendChild(taskDiv);
 
-  btnEvent(taskBtn, obj, taskDiv, arr);
+  btnEvent(taskRemoveBtn, obj, taskDiv, arr);
 };
 
 const btnEvent = (btn, el, div, arr) => {
   btn.addEventListener("click", function () {
     el.removeTask(div);
-    arr.splice(el.index, 1)
-    console.log('posle micanja',arr);
+    arr.splice(el.index, 1);
   });
 };
